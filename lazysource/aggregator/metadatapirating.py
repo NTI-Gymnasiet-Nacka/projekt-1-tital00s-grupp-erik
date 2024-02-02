53a43b0a93250516d810b9149c8625a4b22a532c
EddieE84 <eddie.ekbacke@elev.ga.ntig.se>
2024-02-02 11:50:01 +0100

    test för metadata scraping #1

---
+++ lazysource/aggregator/metadatapirating.py
@@ -0,0 +1,28 @@
+from PyPDF2 import PdfReader
+
+reader = PdfReader("example.pdf")
+
+meta = reader.metadata
+
+print(len(reader.pages))
+
+# All of the following could be None!
+print(meta.author)
+print(meta.creator)
+print(meta.producer)
+print(meta.subject)
+print(meta.title)
+
+if __name__ == "__main__":
+    reader = PdfReader("example.pdf")
+
+    meta = reader.metadata
+
+    print(len(reader.pages))
+
+    # All of the following could be None!
+    print(meta.author)
+    print(meta.creator)
+    print(meta.producer)
+    print(meta.subject)
+    print(meta.title)
