d17aaed66142f0407fe88fe50621ea2b3b6696e9
AantoonnMN <anton.maksymchuknetterstrom@elev.ga.ntig.se>
2024-02-02 13:52:57 +0100

    Issue #4 - Title finder
    Managed to write a function to find title by searching for the text with biggest size/font.
    
    The commented block is for finding authors. FYI, will only work if the name is a line under the keyword Authors. Because of that, it is placed lower in the priority list since the difficulty is too high.

--- lazysource/aggregator/pdftosource.py
+++ lazysource/aggregator/pdftosource.py
@@ -1,5 +1,42 @@
-class PdfToSource:
-    pass
+import pdfplumber
+
+# keyword = 'Authors'
+
+def extract_pdf(pdf_path):
+    
+    with pdfplumber.open(pdf_path) as pdf:
+        first_page = pdf.pages[0]
+        font_groups = {}
+        
+       # Group characters by font size
+        for obj in first_page.chars:
+            font_size = obj['size']
+            text = obj['text']
+            font_groups.setdefault(font_size, []).append(text)
+        
+        # Find the group with the largest font size
+        largest_font_group = max(font_groups.keys())
+        title = ''.join(font_groups[largest_font_group])
+        
+        title = title.strip()
+        
+        return title
+        
+#        for page in pdf.pages[:3]:
+#            text = page.extract_text()
+#            
+#            # Search for keyword
+#            index = text.find(keyword) # Returns -1 if no result found
+#            if index != -1:
+#                # Find the next newline character after the keyword
+#                newline_index = text.find('\n', index)
+#                if newline_index != -1:
+#                    # Extract the line after the keyword
+#                    line = text[newline_index+1:].split('\n')[0].strip()
+#                    return line
+#    return None
 
 if __name__ == "__main__":
-    print("Do you testing here")
+    
+    path = 'C:\\Users\\anton.maksymchuknet\\Desktop\\pdftest\\FULLTEXT01.pdf'
+    print(extract_pdf(path))
