19282feaae2dd1d3ca50a595b7146758363cd69b
Leo A <leo.altebro@elev.ga.ntig.se>
2024-02-07 11:08:16 +0100

    Tested and modified beased on tests #8

--- lazysource/aggregator/newsarticle_to_source.py
+++ lazysource/aggregator/newsarticle_to_source.py
@@ -1,5 +1,4 @@
 import requests
-import re
 from newspaper import Article
 from newspaper.utils import BeautifulSoup
 
@@ -8,31 +7,42 @@ class Newsarticle():
     def __init__(self, base_url) -> None:
         
         self.base_url = base_url
-        self.raw_html = requests.get(base_url)
+        self._raw_html = requests.get(base_url)
         
         self.setup()
         
     def setup(self):
         
-        soup = BeautifulSoup(self.raw_html.text, "html.parser")
+        soup = BeautifulSoup(self._raw_html.text, "html.parser")
         
         article = Article('')
-        article.download(self.raw_html.content)
+        article.download(self._raw_html.content)
         article.parse()
         
         self.title = article.title
+        
         if article.publish_date is not None:
             date_time = str(article.publish_date)
             self.d_o_p = date_time.split()[0]
         else:
             self.d_o_p = None
-        self.authors = article.authors
-        self.publisher = article.meta_data.get("og").get("site_name")
+            
+        self._raw_authors = article.meta_data.get("author")
+        if self._raw_authors in (False, None):
+            self._raw_authors = article.authors
+        if isinstance(self._raw_authors, str):
+            self._raw_authors = [self._raw_authors]
+        self.authors = ''.join([f"{author};" for author in self._raw_authors])
         
-
-#testing stuff, more test need to be done
-base_url = 'https://hard-drive.net/hd/ian-miles-cheong-first-recipient-of-neuralink-thanks-to-inherently-low-risk-of-damaging-intellect/'
-
-new_article = Newsarticle(base_url=base_url)
-print(new_article.authors)
-print(new_article.publisher)
+        self.publisher = None
+        try:
+            self.publisher = article.meta_data.get("article").get("publication")
+        except AttributeError:
+            self.publisher = None
+        if self.publisher is None:
+            try:
+                self.publisher = article.meta_data.get("og").get("site_name")
+            except AttributeError:
+                self.publisher = None
+            if self.publisher is None:
+                self.publisher = article.meta_data.get("application-name")
