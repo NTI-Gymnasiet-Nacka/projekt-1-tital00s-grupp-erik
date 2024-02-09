b12da90b96ca520d32b1834e593002e39ea3348c
eduards <eduards@abishevs.dev>
2024-02-09 13:42:47 +0100

    feat: (#24) started export screen

--- lazysource/app.py
+++ lazysource/app.py
@@ -1,5 +1,13 @@
+from prompt_toolkit import prompt
+
 from lazysource.database.db_manager import DatabaseManager
 
 class App:
     def __init__(self):
         self.db_manager = DatabaseManager()
+
+
+    def export_source(self):
+        options = ["All good"]
+
+
