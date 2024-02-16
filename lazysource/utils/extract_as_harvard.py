44af71eb368fd3e283e274d574355dc71b1aedd6
EddieE84 <eddie.ekbacke@elev.ga.ntig.se>
2024-02-16 10:34:56 +0100

    Update extract_as_harvard.py
    #7 fixade artikel export

--- lazysource/utils/extract_as_harvard.py
+++ lazysource/utils/extract_as_harvard.py
@@ -1,4 +1,4 @@
-def author_format(names: list):
+def author_format(names: list) -> str:
     formated_auhtors_list = []
     for name in names:
         formated_name_list = []
@@ -55,31 +55,58 @@ def harvard_format_book(d: dict) -> str:
     else:
         publsiher = d.get("publisher")
         
-    if d.get("page_nums") == None:
-        raise ValueError("Page numbers is None")
-    else:
-        page_nums = d.get("page_nums")
-        
     if d.get("edition") == None:
         raise ValueError("Edition is None")
     else:
         edition = d.get("edition")
+       
+    y_o_p = d_o_p.split("-")[-1]
+    
+    if d.get("page_nums") != None:
+        page_nums = d.get("page_nums")
+        return f"{authors} ({y_o_p}). {title}. {edition}. edition. {publsiher}, {page_nums}."
+    else:
+        return f"{authors} ({y_o_p}). {title}. {edition}. edition. {publsiher}."
+    
+
+def harvard_format_article(d:dict) -> str:
+    if d.get("title") == None:
+        raise ValueError("Title is None")
+    else:
+        title = d.get("title")
+    
+    if d.get("d_o_p") == None:
+        raise ValueError("d_o_p is None")
+    else:
+        d_o_p = d.get("d_o_p")
+    
+    if d.get("authors") == None:
+        raise ValueError("Author is None")
+    else:
+        authors = author_format(d.get("authors"))
+    
+    if d.get("publisher") == None:
+        raise ValueError("Publisher is None")
+    else:
+        publsiher = d.get("publisher")
+    
+    if d.get("url") == None:
+        raise ValueError("url is None")
+    else:
+        url = d.get("url")
+        
+    if d.get("access_date") == None:
+        raise ValueError("Access_date is None")
+    else:
+        access_date = d.get("access_date")
     
     y_o_p = d_o_p.split("-")[-1]
-    return f"{authors} ({y_o_p}). {title}. {edition}. edition. {publsiher}."
-            
-if __name__ == "__main__":
-    source_data = { 
-    "category": "book",
-    "title":  "500 skämt om papperssortering",
-    "d_o_p":  "12-12-2012",
-    "authors": ["Vidar Silas Mörk", "Eddie Atilla Ekbacke", "William Carl Svensson"],
-    "publisher":  "Bonnier",
-    "page_nums":  "21-22",
-    "edition": "5",
-    "url": "www.youtube.com/video",
-    "access_date":  "12-11-2012"
-}
+    d_m = d_o_p.split("-")[0] + "-" + d_o_p.split("-")[1]
+    
+    return f"{authors} ({y_o_p}). {title}. {publsiher}. {d_m}. {url} ({access_date})."
+    
+  
+def build_export_string(source_data:dict) -> str:
     if source_data.get("category") == None:
         raise ValueError("Category is None")
     else:
@@ -87,5 +114,26 @@ if __name__ == "__main__":
     
     if category == "book":
         res = harvard_format_book(source_data)
-        print(res)
+        return res
+    
+    elif category == "article":
+        res = harvard_format_article(source_data)
+        return res
+
+            
+if __name__ == "__main__":
+    source_data = { 
+    "category": "article",
+    "title":  "500 skämt om papperssortering",
+    "d_o_p":  "12-12-2012",
+    "authors": ["Vidar Silas Mörk", "Eddie Ekbacke", "William Carl Svensson"],
+    "publisher":  "Bonnier",
+    "page_nums":  "21-22",
+    "edition": "5",
+    "url": "www.youtube.com/video",
+    "access_date": "2012-11-21"
+}
+    res = build_export_string(source_data)
+    print(res)
+    
    
